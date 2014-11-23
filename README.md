e0af6077e614974e6e0edda5333c23cb8a038e99
schwalb <jeremy.schwalb@gmail.com>
2014-11-23 16:19:19 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-GettingCleaningData
-===================
+#Introduction
+
+This repository is hosting the R code for the project of the DataScience track's "Getting and Cleaning Data" course.
+
+The purpose of this project is to collect and clean a data set. Tidy data have been prepared for later analysis.
+
+#Data Set
+
+The data set "Human Activity Recognition Using Smartphones" has been taken from UCI.
+
+#Execution and files
+
+The Data Set has been stored in UCI HAR Dataset/ directory.
+
+The CodeBook.md describes the variables, the data, and the work that has been performed to clean up the data.
+
+The run_analysis.R is the script that has been used for this work. It can be loaded in R/Rstudio and executed.
+
+The result of the script execution is that a tidy.csv file is being created.
--- run_analysis.R
+++ run_analysis.R
@@ -6,12 +6,12 @@
   unzip("Dataset.zip")
 
 #take all the data and set a them a variable
-test_X <- read.table("./UCI HAR Dataset/test/X_test.txt",header=FALSE)
-test_Y <- read.table("./UCI HAR Dataset/test/y_test.txt",header=FALSE)
-test_sub <- read.table("./UCI HAR Dataset/test/subject_test.txt",header=FALSE)
-train_X <- read.table("./UCI HAR Dataset/train/X_train.txt",header=FALSE)
-train_Y <- read.table("./UCI HAR Dataset/train/y_train.txt",header=FALSE)
-train_sub <- read.table("./UCI HAR Dataset/train/subject_train.txt",header=FALSE)
+  test_X <- read.table("./UCI HAR Dataset/test/X_test.txt",header=FALSE)
+  test_Y <- read.table("./UCI HAR Dataset/test/y_test.txt",header=FALSE)
+  test_sub <- read.table("./UCI HAR Dataset/test/subject_test.txt",header=FALSE)
+  train_X <- read.table("./UCI HAR Dataset/train/X_train.txt",header=FALSE)
+  train_Y <- read.table("./UCI HAR Dataset/train/y_train.txt",header=FALSE)
+  train_sub <- read.table("./UCI HAR Dataset/train/subject_train.txt",header=FALSE)
 
 # Create data with the activities type
   activities <- read.table("./UCI HAR Dataset/activity_labels.txt",header=FALSE)
@@ -21,25 +21,25 @@ train_sub <- read.table("./UCI HAR Dataset/train/subject_train.txt",header=FALSE
   features <- read.table("./UCI HAR Dataset/features.txt",header=FALSE)
 
 #Set good columns names to data frames viewed above
-colnames(test_X)<-features$V2
-colnames(train_X)<-features$V2
-colnames(test_Y)<-c("Activity")
-colnames(train_Y)<-c("Activity")
-colnames(test_sub)<-c("Subject")
-colnames(train_sub)<-c("Subject")
+  colnames(test_X)<-features$V2
+  colnames(train_X)<-features$V2
+  colnames(test_Y)<-c("Activity")
+  colnames(train_Y)<-c("Activity")
+  colnames(test_sub)<-c("Subject")
+  colnames(train_sub)<-c("Subject")
 
-# 1. merge test and training sets into one data set, including the activities
-testData<-cbind(test_X,test_Y)
-testData<-cbind(testData,test_sub)
-trainData<-cbind(train_X,train_Y)
-trainData<-cbind(trainData,train_sub)
-Data<-rbind(testData,trainData)
+# merge test and training sets into one data set, including the activities
+  testData<-cbind(test_X,test_Y)
+  testData<-cbind(testData,test_sub)
+  trainData<-cbind(train_X,train_Y)
+  trainData<-cbind(trainData,train_sub)
+  Data<-rbind(testData,trainData)
 
-# 2. extract only the measurements on the mean and standard deviation for each measurement
+# extract the mean and standard deviation for each measurement
 Data_mean<-sapply(Data,mean,na.rm=TRUE)
 Data_sd<-sapply(Data,sd,na.rm=TRUE)
 
-# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
   install.packages("data.table")
   library(data.table)
   DT <- data.table(Data)
